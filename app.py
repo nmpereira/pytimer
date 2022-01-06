90e50dee56c9a141a36caf19d3ea3e1859d14012
nmpereira <nikhilpereira25@gmail.com>
2022-01-06 00:41:05 -0500

    update timer in work and rest page to hh:mm:ss

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 
+
 app = Flask(__name__)
 app.secret_key="abc"
 app.debug = True
@@ -25,13 +26,20 @@ def home():
         session["rest"] = rest
         session["sets"] = sets
         session["set_counter"] = 0
+        
+        # session["rest_formatted"]=datetime.timedelta(seconds=rest)
         return redirect(url_for("excercise"))
     return render_template("home.html")
+
+def to_hms(s):
+    m, s = divmod(s, 60)
+    h, m = divmod(m, 60)
+    return '{}:{:0>2}:{:0>2}'.format(h, m, s)
 
 
 @app.route("/rest")
 def rest():
-    return render_template("rest.html", rest=session["rest"])
+    return render_template("rest.html", rest=session["rest"], rest_formatted=to_hms(session["rest"]))
 
 
 
@@ -41,10 +49,11 @@ def complete():
 
 @app.route("/excercise")
 def excercise():
+    
     if session["set_counter"] ==session["sets"]:
         return redirect(url_for("complete"))
     session["set_counter"] +=1
-    return render_template("excercise.html",excercise=session["excercise"])
+    return render_template("excercise.html",excercise=session["excercise"], excercise_formatted=to_hms(session["excercise"]))
 
 if __name__ == '__main__':
     app.run()
